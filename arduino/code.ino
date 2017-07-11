9ca7fc982537e0584ce8d44b8fc2e63a2c37c6cc
fabio.molliet <fmolliet@hotmail.com>
2017-07-11 12:16:25 -0300

    Ultra FIX Meus Bons
    Codigo lisão pra rodar saporra

--- arduino/code.ino
+++ arduino/code.ino
@@ -1,11 +1,41 @@
+/*
+		Robo: Caubi 
+		Versão: 0.1
+		Data:11/07/2017 12:13
+	
+		obs: Sempre anotar a data da edição Meu Caros,
+		se for uma pequena complementação soma a versão: +X.1 , se for grande +1.X
+		Vou deixar esse espaço de complementação e comentários
+*/
+
+// Falta biblioteca do motor // entrada e saida de dados ?
+
+
+// Declaração de Variaveis úteis
+
+char dataIn = 'S';
+char determinant;
+int vel = 0					// coisa do bluetooth
 char data = 0;            //Variavel para armazenar dados
+
+
+
 void setup()
 {
     Serial.begin(9600);   //Prepara o baud para transmissão de dados em serial                            
     pinMode(13, OUTPUT);  //Coloca o pin 13 como saida
+	// precisa ver isso no motor que agente comprar
+	// pode mudar \/
+	motorRight.setSpeed(0);
+	motorLeft.setSpeed(0);
+	motorRight.run(RELEASE);
+	motorLeft.run(RELEASE);
 }
 void loop()
 {
+   data = check();
+
+		/* If para ligar led (TESTE)
    if(Serial.available() > 0)      // manda dados somente quando receber dados:
    {
       data = Serial.read();        //ler os dados que vieram e armazena em data
@@ -16,4 +46,76 @@ void loop()
       else if(data == '0')         // Checa quando valor de data é igual a 0
          digitalWrite(13, LOW);    //Se o valor é 0 então LED desliga
    }
+   */
+   /*
+   
+			Precisa Rever Tudo depois 
+   
+	*/
+    switch (det){
+		case 'F': // F, move para frente
+    motorRight.setSpeed(vel);
+    motorLeft.setSpeed(vel);
+    motorRight.run(FORWARD);      
+    motorLeft.run(FORWARD);
+    det = check();
+    break;
+    
+    case 'T': // T move para tras
+    motorRight.setSpeed(vel);
+    motorLeft.setSpeed(vel);
+    motorRight.run(BACKWARD);      
+    motorLeft.run(BACKWARD);
+    det = check();
+    break;
+    
+    case 'E':// E vai para esquerda
+    motorRight.setSpeed(vel);
+    motorLeft.setSpeed(vel/4);
+    motorRight.run(FORWARD);      
+    motorLeft.run(FORWARD);
+    det = check();
+    break;
+    
+    case 'D': // D vai pra direira
+    motorRight.setSpeed(vel/4);
+    motorLeft.setSpeed(vel);
+    motorRight.run(FORWARD);      
+    motorLeft.run(FORWARD);
+    det = check();
+    break;
+	
+	case 'P': // P, para
+    motorRight.setSpeed(vel);
+    motorLeft.setSpeed(vel);
+    motorRight.run(RELEASE);      
+    motorLeft.run(RELEASE);
+    det = check();
+    break;
+	}
+	// pega o codigo do bluetooth pela porta serial
+	int check(){
+  if (Serial.available() > 0){// Se for uma variavel valida do serial
+    dataIn = Serial.read();// guarda o valor da variavel
+    
+    // Checa o codigo
+    if (dataIn == 'F'){//Frente
+      determinant = 'F';
+    }
+    else if (dataIn == 'T'){//para Trás
+      determinant = 'T';
+    }
+    else if (dataIn == 'E'){//Esquerda
+      determinant = 'E';
+    }
+    else if (dataIn == 'D'){//Right
+      determinant = 'D';
+    }
+	else if (dataIn == 'S'){//Stop
+      determinant = 'S';
+    }
+  }
+}
+   return determinant;
+
 }
