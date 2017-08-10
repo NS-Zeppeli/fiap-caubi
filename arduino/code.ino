a5b62715463a4df5e1c132b9e6f8f6976214a283
fabio.molliet <fmolliet@hotmail.com>
2017-08-10 13:23:58 -0300

    Caubi v1.0.1
    Add Motores+ biblioteca+ comandos ao andar trocados por Functions // comandos antigos ainda estão no codigo // no final

--- arduino/code.ino
+++ arduino/code.ino
@@ -1,7 +1,7 @@
 /*
 		Robo: Caubi 
-		Versão: 0.3.1
-		Data:12/07/2017 8:59
+		Versão: 1.0.1
+		Data:10/08/2017 13:19
 		                                                                                       
         CCCCCCCCCCCCC               AAA           UUUUUUUU     UUUUUUUUBBBBBBBBBBBBBBBBB   IIIIIIIIII
      CCC::::::::::::C              A:::A          U::::::U     U::::::UB::::::::::::::::B  I::::::::I
@@ -27,12 +27,14 @@ C:::::C                   A:::::::::::::::::::::A  U:::::D     D:::::U   B::::B 
 */
 
 // Bibliotecas 
-#include <> // Falta biblioteca do motor
+#include <AFMotor.h> // biblioteca do motor
 #include <Arduino.h>
 
 // declaração de objetos (Motores) // preciso da documentação e arquivos do motor 
-<nome do motor na biblioteca> motorRight(1, ); 
-<nome do motor na biblioteca> motorLeft(3, );  
+AF_DCMotor motorRight1(1); 
+AF_DCMotor motorRight2(2);
+AF_DCMotor motorLeft1(3);
+AF_DCMotor motorLeft2(4); // o numero interno é a porta no shield XD
 
 // Declaração de Variaveis úteis
 
@@ -40,7 +42,7 @@ char dataIn = 'P';  // começa parado
 char determinante;
 int vel = 0;					// coisa do bluetooth
 char data = 0;            //Variavel para armazenar dados
-
+const int Ledligado = 6; // liga led
 
 void setup()
 {
@@ -52,14 +54,140 @@ void setup()
 	motorLeft.setSpeed(0);
 	motorRight.run(RELEASE);
 	motorLeft.run(RELEASE);
+	pinMode(Ledligado, OUTPUT);
 }
 
 
 void loop()
 {
-   data = check();
+	if(Serial.available() > 0){ 
+		command = Serial.read(); 
+		parado(); //Inicializa os motores parados
+		//Serial.println(command); // printa comando para testar entrada
+		switch(command){
+		case 'F':  
+		  Frente();
+		  break;
+		case 'T':  
+		   tras();
+		  break;
+		case 'E':  
+		  esquerda();
+		  break;
+		case 'D':
+		  direita();  
+		  break;
+		  /*
+		case 'W':
+		  digitalWrite(6,HIGH);
+		  break;
+		case 'w':
+		  digitalWrite(6,LOW);
+		  break;
+		  */
+		 }
+	  } 
+		
+}
+	
+	
+	void frente() // vai para frente 
+	{
+	  motorRight1.setSpeed(90);
+	  motorRight1.run(FORWARD); 
+	  motorRight2.setSpeed(90);
+	  motorRight2.run(FORWARD);
+	  motorLeft1.setSpeed(90);
+	  motorLeft1.run(FORWARD); 
+	  motorLeft2.setSpeed(90);
+	  motorLeft2.run(FORWARD);
+	}
 
-		/* If para ligar led (TESTE)
+	void tras()
+	{
+	  motorRight1.setSpeed(90); 
+	  motorRight1.run(BACKWARD); //roda motor sentido anti-horario
+	  motorRight2.setSpeed(90); 
+	  motorRight2.run(BACKWARD); 
+	  motorLeft1.setSpeed(90); 
+	  motorLeft1.run(BACKWARD); //roda motor sentido anti-horario
+	  motorLeft2.setSpeed(90); 
+	  motorLeft2.run(BACKWARD);
+	}
+
+	void esquerda()
+	{
+	  motorRight1.setSpeed(90); // coloca velociade maxima
+	  motorRight1.run(FORWARD);
+	  motorRight2.setSpeed(90); 
+	  motorRight2.run(FORWARD);	  
+	  motorLeft1.setSpeed(0); // desliga motor 2
+	  motorLeft1.run(RELEASE);
+	  motorLeft2.setSpeed(0); 
+	  motorLeft2.run(RELEASE);	  
+	}
+
+	void direita()
+	{
+	  motorRight1.setSpeed(0); //Desliga motor 1
+	  motorRight2.setSpeed(0);
+	  motorRight1.run(RELEASE);
+	  motorRight2.run(RELEASE);
+	  motorLeft1.setSpeed(90);
+	  motorLeft2.setSpeed(90);
+	  motorLeft1.run(FORWARD); 
+	  motorLeft2.run(FORWARD);
+	}
+
+	void parado()// para motores
+	{
+	  motorRight1.setSpeed(0);
+	  motorRight2.setSpeed(0);
+	  motorRight1.run(RELEASE); 
+	  motorRight2.run(RELEASE);
+	  motorLeft1.setSpeed(0);
+	  motorLeft2.setSpeed(0);
+	  motorLeft1.run(RELEASE);
+	  motorLeft2.run(RELEASE);
+	}
+	
+	/*	
+ _______  _____  ______  _____  ______  _____  _______      _______ __   _ _______ _____  ______  _____  _______
+ |       |     | |     \   |   |  ____ |     | |______      |_____| | \  |    |      |   |  ____ |     | |______
+ |_____  |_____| |_____/ __|__ |_____| |_____| ______|      |     | |  \_|    |    __|__ |_____| |_____| ______|
+	
+	// pega o codigo do bluetooth pela porta serial
+	int check(){
+  if (Serial.available() > 0){// Se for uma variavel valida do serial
+    dataIn = Serial.read();// guarda o valor da variavel
+    
+    // Checa o codigo
+    if (dataIn == 'F'){//Frente
+      determinante = 'F';
+    }
+    else if (dataIn == 'T'){//para Trás
+      determinante = 'T';
+    }
+    else if (dataIn == 'E'){//Esquerda
+      determinante = 'E';
+    }
+    else if (dataIn == 'D'){//Right
+      determinante = 'D';
+    }
+	else if (dataIn == 'S'){//Stop
+      determinante = 'S';
+    }
+  }
+}
+   return determinant;
+
+}
+
+
+
+
+Voidloop{
+	/* If para ligar led (TESTE)
    if(Serial.available() > 0)      // manda dados somente quando receber dados:
    {
       data = Serial.read();        //ler os dados que vieram e armazena em data
@@ -76,6 +204,7 @@ void loop()
 			Precisa Rever Tudo depois 
    
 	*/
+	/*
     switch (dataIn){
 		case 'F': // F, move para frente
 		motorRight.setSpeed(vel);
@@ -116,30 +245,13 @@ void loop()
 		motorLeft.run(RELEASE);
 		data = check();
 		break;
-	}
-	// pega o codigo do bluetooth pela porta serial
-	int check(){
-  if (Serial.available() > 0){// Se for uma variavel valida do serial
-    dataIn = Serial.read();// guarda o valor da variavel
-    
-    // Checa o codigo
-    if (dataIn == 'F'){//Frente
-      determinante = 'F';
-    }
-    else if (dataIn == 'T'){//para Trás
-      determinante = 'T';
-    }
-    else if (dataIn == 'E'){//Esquerda
-      determinante = 'E';
-    }
-    else if (dataIn == 'D'){//Right
-      determinante = 'D';
-    }
-	else if (dataIn == 'S'){//Stop
-      determinante = 'S';
-    }
-  }
+		*/
 }
-   return determinant;
+*/
 
-}
+
+
+
+
+
+
